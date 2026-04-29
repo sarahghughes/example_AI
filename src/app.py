dcbdc9fef36f7e8854864751b439c1256c69d5f5
Sarah Hughes <sarahhughes4462@gmail.com>
2026-04-28 21:42:39 -0400

    Add 'GenderClean' and 'Race' to excluded columns

--- src/app.py
+++ src/app.py
@@ -313,7 +313,7 @@ if st.session_state.get("all_approved"):
     df        = st.session_state["df"]
     exclude   = {"PersonKey","POC","LowIncome","IsYouth","Female","HighEng",
                "HouseIncomeRaw","HouseIncome","ProgramGroup","YouthAdult",
-               "ProgramCategory","Program", "Age"}
+               "ProgramCategory","Program", "Age", "GenderClean", "Race"}
     data_level  = st.radio("Data level", ["Person-level","Enrollment-level"], horizontal=True)
     src         = df_people if data_level == "Person-level" else df
     col_options = [c for c in src.columns if c not in exclude]
